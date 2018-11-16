f2824c4d30464369e5ca08dabe04717df1de11d8
dkarten <dkarten@crowdtwist.com>
2018-11-16 14:35:45 -0500

    Try res.end()

--- backend/api/controllers/status_controller.js
+++ backend/api/controllers/status_controller.js
@@ -248,7 +248,7 @@ exports.handle_slack_message = function(req, res, next) {
         token = token[0];
 
         if (payload.event !== undefined) {
-          res.json({ok:true});
+            res.status(200).end();
             if (payload.event.type === 'message' && payload.event.text !== '') {
                 var message = payload.event.text.toLowerCase();
                 if (message.includes("qa") || message.includes("staging")) {
