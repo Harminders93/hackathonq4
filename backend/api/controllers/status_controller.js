0506d33fc7bc2d7078b7511d5240da7fb7164cc3
Lucky <hsingh@crowdtwist.com>
2018-11-16 13:26:08 -0500

    RUC-
    Tests:

--- backend/api/controllers/status_controller.js
+++ backend/api/controllers/status_controller.js
@@ -267,7 +267,7 @@ exports.handle_slack_message = function(req, res, next) {
                     response_text = 'Click here to find out ya goober - https://is-qa-free.herokuapp.com/';
 
                     var request = require('request');
-                    token = 'Bearer' + token;
+                    token = 'Bearer' + token.token;
                     console.log(token);
                     request({
                         uri: 'https://slack.com/api/chat.postMessage',
